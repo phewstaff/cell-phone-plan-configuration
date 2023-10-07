31016616a9b0d1ffb75d6b5dffde1c88ae09606d
magomed_n <naurbaev2020@gmail.com>
2023-10-07 10:44:04 +0300

    initial setup

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+// Утилита для динамической стилизации
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
